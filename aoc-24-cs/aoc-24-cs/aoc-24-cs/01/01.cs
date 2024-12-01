82f49ee6aab44fa971486b8e0cd02d54c6d80388
Bjorn Lind <bjorn_92@yahoo.se>
2024-12-01 12:07:55 +0100

    01

---
+++ aoc-24-cs/aoc-24-cs/aoc-24-cs/01/01.cs
@@ -0,0 +1,32 @@
+﻿namespace aoc_24_cs
+{
+    public partial class Program
+    {
+        public static void Problem01()
+        {
+            List<int> leftList = new List<int>();
+            List<int> rightList = new List<int>();
+            
+            foreach (var line in File.ReadLines("01\\input_01.txt"))
+            {
+                var firstNumLen = line.IndexOf(' ');
+                leftList.Add(int.Parse(line.Substring(0, firstNumLen)));
+
+                var sndNumIdx = line.LastIndexOf(" ") + 1;
+                rightList.Add(int.Parse(line.Substring(sndNumIdx)));
+            }
+
+            leftList.Sort();
+            rightList.Sort();
+
+            var totalDist = leftList.Zip(rightList, (ai, bi) => Math.Abs(ai - bi)).Sum();
+
+            Console.WriteLine(totalDist);
+
+            int similarityScore = 0;
+            leftList.ForEach(x => similarityScore += x * rightList.Count(y => x == y));
+
+            Console.WriteLine(similarityScore);
+        }
+    }
+}
---
+++ aoc-24-cs/aoc-24-cs/aoc-24-cs/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace aoc_24_cs
+{
+    public partial class Program
+    {
+        public static void Main(string[] args)
+        {
+            Problem01();
+        }
+    }
+}
