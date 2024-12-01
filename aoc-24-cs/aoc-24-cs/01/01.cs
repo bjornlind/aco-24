8bcb6cee68ad3390d18ee495d5b71338671c0ae4
Bjorn Lind <bjorn_92@yahoo.se>
2024-12-01 13:15:29 +0100

    add cmd args and execution time

--- aoc-24-cs/aoc-24-cs/01/01.cs
+++ aoc-24-cs/aoc-24-cs/01/01.cs
@@ -1,32 +1,38 @@
-﻿namespace aoc_24_cs
+﻿using System.Diagnostics;
+
+namespace aoc_24_cs
 {
     public partial class Program
     {
         public static void Problem01()
         {
+            var sw = Stopwatch.StartNew();
             List<int> leftList = new List<int>();
             List<int> rightList = new List<int>();
             
             foreach (var line in File.ReadLines("01\\input_01.txt"))
             {
-                var firstNumLen = line.IndexOf(' ');
-                leftList.Add(int.Parse(line.Substring(0, firstNumLen)));
-
-                var sndNumIdx = line.LastIndexOf(" ") + 1;
-                rightList.Add(int.Parse(line.Substring(sndNumIdx)));
+                var nums = line.Split("   ");
+                leftList.Add(int.Parse(nums[0]));
+                rightList.Add(int.Parse(nums[1]));
             }
 
             leftList.Sort();
             rightList.Sort();
 
             var totalDist = leftList.Zip(rightList, (ai, bi) => Math.Abs(ai - bi)).Sum();
+            var timeTaken1 = sw.Elapsed;
 
             Console.WriteLine("Day 01-01: " + totalDist);
+            Console.WriteLine("Execution time (ms): " + timeTaken1.TotalMilliseconds);
+
 
             int similarityScore = 0;
             leftList.ForEach(x => similarityScore += x * rightList.Count(y => x == y));
+            var timeTaken2 = sw.Elapsed - timeTaken1;
 
             Console.WriteLine("Day 01-02: " + similarityScore);
+            Console.WriteLine("Execution time (ms): " + timeTaken2.TotalMilliseconds);
         }
     }
 }
--- aoc-24-cs/aoc-24-cs/Program.cs
+++ aoc-24-cs/aoc-24-cs/Program.cs
@@ -2,9 +2,37 @@
 {
     public partial class Program
     {
+        private static readonly int Days = 25;
+
+        private static List<Action> actions = new List<Action>()
+        {
+            Problem01
+        };
+
         public static void Main(string[] args)
         {
-            Problem01();
+            if(args.Length > 0 && int.TryParse(args[0], out int problem))
+            {
+                if(problem > Days || problem < 1)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(args), problem,
+                        $"Invalid input, select between day 1 to 25.");
+                }
+
+                if(problem > actions.Count)
+                {
+                    Console.WriteLine($"Day {problem} not solved yet ...");
+                    return;
+                }
+
+                Console.WriteLine($"Solving day {problem} ...");
+                actions[problem - 1].Invoke();
+            }
+            else
+            {
+                Console.WriteLine($"Solving day {actions.Count} ...");
+                actions.Last().Invoke();
+            }
         }
     }
 }
