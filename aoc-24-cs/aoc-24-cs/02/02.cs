0f79380afe916a8961a05a9e98398f57ea5db35d
Bjorn Lind <bjorn_92@yahoo.se>
2024-12-02 19:26:25 +0100

    Day 2 problem 2

--- aoc-24-cs/aoc-24-cs/02/02.cs
+++ aoc-24-cs/aoc-24-cs/02/02.cs
@@ -9,23 +9,20 @@ namespace aoc_24_cs
         {
             var sw = Stopwatch.StartNew();
             int safeReports = 0;
-            
+            int safeReportsWithDamper = 0;
+
             foreach (var line in File.ReadLines("02\\input_02.txt"))
             {
                 var nums = line.Split(" ").Select(str => int.Parse(str)).ToList();
                 safeReports += CheckReport(nums) ? 1 : 0;
+                safeReportsWithDamper += CheckReportWithDamper(nums) ? 1 : 0;
             }
 
-            var timeTaken1 = sw.Elapsed;
+            var timeTaken = sw.Elapsed;
 
             Console.WriteLine("Day 02-01: " + safeReports);
-            Console.WriteLine("Execution time (ms): " + timeTaken1.TotalMilliseconds);
-
-
-            //var timeTaken2 = sw.Elapsed - timeTaken1;
-
-            //Console.WriteLine("Day 01-02: " + similarityScore);
-            //Console.WriteLine("Execution time (ms): " + timeTaken2.TotalMilliseconds);
+            Console.WriteLine("Day 02-02: " + safeReportsWithDamper);
+            Console.WriteLine("Execution time (ms): " + timeTaken.TotalMilliseconds);
         }
 
         private static bool CheckReport(List<int> report)
@@ -51,6 +48,29 @@ namespace aoc_24_cs
             }
 
             return true;
+        }
+
+        private static bool CheckReportWithDamper(List<int> report)
+        {
+            if (CheckReport(report))
+            {
+                return true;
+            }
+            else
+            {
+                for (int i = 0; i < report.Count; i++)
+                {
+                    List<int> reducedList = new List<int>(report);
+                    reducedList.RemoveAt(i);
+                    if (CheckReport(reducedList))
+                    {
+                        Console.WriteLine($"Remove {report[i]} from: [{string.Join(' ', report)}]");
+                        return true;
+                    }
+                }
+            }
+
+            return false;
         }
     }
 }
