dd26777f85d6c257a8d961ad94c5186f148c6c27
Björn Lind <bjornli@axis.com>
2024-12-03 18:17:55 +0100

    Day 3 problem 2

--- aoc-24-cs/aoc-24-cs/03/03.cs
+++ aoc-24-cs/aoc-24-cs/03/03.cs
@@ -9,34 +9,57 @@ namespace aoc_24_cs
         {
             var sw = Stopwatch.StartNew();
             int sum = 0;
+            int sum2 = 0;
 
-            foreach (var line in File.ReadLines("03\\input_03.txt"))
-                //foreach (var line in File.ReadLines("03\\example_03.txt"))
+            string input = File.ReadAllText("03\\input_03.txt");
+            
+            // Part 1
+            sum += ExtractSum(input);
+
+            // Part 2
+            var donts = input.Split("don't()");
+
+            for (int i = 0; i < donts.Length; i++)
             {
-                var matches = Regex.Matches(line, "mul\\([0-9]{1,3},[0-9]{1,3}\\)");
-
-                foreach (var match in matches)
+                if(i == 0)
                 {
-                    var str = match.ToString();
-                    var comma = str.IndexOf(',');
-                    var num1 = int.Parse(str.Substring(4, comma - 4));
-                    var num2 = int.Parse(str.Substring(comma + 1, str.Length - 2 - comma));
+                    sum2 += ExtractSum(donts[i]);
+                }
+                else
+                {
+                    int doIdx = donts[i].IndexOf("do()");
+                    if (doIdx > -1)
+                    {
+                        string doStr = donts[i].Substring(doIdx);
+                        sum2 += ExtractSum(doStr);
 
-                    sum += num1 * num2;
-                    Console.WriteLine(match.ToString());
+                    }
                 }
             }
 
-            Console.WriteLine(sum);
-
             var timeTaken1 = sw.Elapsed;
 
-            //Console.WriteLine("Day 01-01: " + totalDist);
+            Console.WriteLine("Day 03-01: " + sum);
+            Console.WriteLine("Day 03-02: " + sum2);
             Console.WriteLine("Execution time (ms): " + timeTaken1.TotalMilliseconds);
+        }
 
-            var timeTaken2 = sw.Elapsed - timeTaken1;
+        private static int ExtractSum(string line)
+        {
+            var matches = Regex.Matches(line, "mul\\([0-9]{1,3},[0-9]{1,3}\\)");
+            int sum = 0;
 
-            Console.WriteLine("Execution time (ms): " + timeTaken2.TotalMilliseconds);
+            foreach (var match in matches)
+            {
+                var str = match.ToString();
+                var comma = str.IndexOf(',');
+                var num1 = int.Parse(str.Substring(4, comma - 4));
+                var num2 = int.Parse(str.Substring(comma + 1, str.Length - 2 - comma));
+
+                sum += num1 * num2;
+            }
+
+            return sum;
         }
     }
 }
